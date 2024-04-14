70710aaba363314b279eb286dafe9f80e4b985b9
bmg-c <letalbark@gmail.com>
2024-04-14 21:26:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import customtkinter
-
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("400x240")
+from tkinter import Event
 
 
-def button_function():
-    print("button pressed")
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    def __init__(self, master, values):
+        super().__init__(master)
+        self.values = values
+        self.checkboxes = []
+
+        for i, value in enumerate(self.values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.bind("<B1-Motion>", self.hello)
+        self.title("my app")
+        self.geometry("400x180")
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.checkbox_frame_1 = MyCheckboxFrame(self, values=["value 1", "value 2", "value 3"])
+        self.checkbox_frame_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
+        self.checkbox_frame_2 = MyCheckboxFrame(self, values=["option 1", "option 2"])
+        self.checkbox_frame_2.grid(row=0, column=1, padx=(0, 10), pady=(10, 0), sticky="nsew")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+
+    def hello(self, event: Event):
+        print(event.x_root)
+
+    def button_callback(self):
+        print("checkbox_frame_1:", self.checkbox_frame_1.get())
+        print("checkbox_frame_2:", self.checkbox_frame_2.get())
 
 
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-
+app = App()
 app.mainloop()
